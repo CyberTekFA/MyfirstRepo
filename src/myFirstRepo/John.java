e2dcb56706f586d81a287ce5e703bdcf1e119ac4
CyberTekFA <faiz.m.amiree@gmail.com>
2019-03-18 13:01:49 -0400

    calculator2

--- src/myFirstRepo/John.java
+++ src/myFirstRepo/John.java
@@ -3,6 +3,25 @@ package myFirstRepo;
 public class John {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+	
+		System.out.println(addition(3,2));
+		System.out.println(multiply(5,3));
+		System.out.println(division(25,3));
 	}
+	
+	public static int addition(int num1 , int num2) {
+		return num1+num2;
+	}
+	
+	public static int multiply(int num1 , int num2) {
+		return num1*num2;
+	}
+	public static int subtraction(int num1 , int num2) {
+		return num1-num2;
+	}
+	
+	public static int division(int num1 , int num2) {
+		return num1/num2;
+	}
+	
 }
